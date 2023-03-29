dfff011d424b536df33736f9e52377a37b027cb9
Vipul <rudradalal0082004@gmail.com>
2023-03-29 10:05:34 +0530

    update

--- js/main.js
+++ js/main.js
@@ -27,3 +27,30 @@ links3.addEventListener('click', linkFunc);
 function linkFunc(e){
   e.preventDefault();
 }
+
+
+// Send Subscriber Emails to Google sheets
+const scriptURL = 'https://script.google.com/macros/s/AKfycby5LDUy_xNXm0uPqZvZoRWzoSSxTIbDzLNP5LrWo9vMQl8WeL3aMaJbl9S728Ug2xEHOQ/exec'
+    const form = document.forms['submit-to-google-sheet']
+    const msg = document.getElementById('msg');
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+          msg.innerHTML = 'Thank You for Subscribing!';
+          setTimeout(() => {
+            msg.innerHTML = ' ';
+          }, 3000);
+          form.reset();
+        })
+        .catch(error => console.error('Error!', error.message))
+})
+
+
+// Form reset after submiting
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName('form')) {
+    form.reset();
+  }
+}
